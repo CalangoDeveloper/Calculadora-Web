1913f26ba2e8e79d5f846647ebbe40c5ddc40484
CalangoDeveloper <152935344+CalangoDeveloper@users.noreply.github.com>
2024-04-02 16:05:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function addValor(a){
+    document.getElementById('resultado').innerHTML += a
+};
+function resu(a){
+    document.getElementById('resultado').innerHTML = eval(document.getElementById('resultado').innerHTML)
+};
+function apagar(a){
+    document.getElementById('resultado').innerHTML = null
+};
